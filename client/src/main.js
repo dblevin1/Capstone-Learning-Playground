81201944b507bf85e0aedb7c28e2b0049a04b592
Daniel Blevins <dblevin1@asu.edu>
2020-10-04 20:33:38 -0700

    Changed CRLF ending to LF

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,4 @@
-import Vue from 'vue';
+/*import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
@@ -7,4 +7,21 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount('#app');*/
+import Vue from 'vue';
+import Router from './router';
+import Ping from './components/Ping.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/ping',
+      name: 'Ping',
+      component: Ping,
+    }
+  ],
+});
